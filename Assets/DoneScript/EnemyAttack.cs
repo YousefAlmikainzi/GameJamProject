2a6e2091d2b05b3cd4dd75615f59a196ddb32ad1
Yuzu1999 <63368986+YousefAlmikainzi@users.noreply.github.com>
2025-11-01 11:51:04 +0300

    soldierTested

--- Assets/DoneScript/EnemyAttack.cs
+++ Assets/DoneScript/EnemyAttack.cs
@@ -5,11 +5,11 @@ public class EnemyAttack : MonoBehaviour
     [Header("Refs")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] Transform player;
-    [SerializeField] EnemyType enemyType;         
+    [SerializeField] EnemyType enemyType;
 
     [Header("Firing")]
-    [SerializeField] float fireInterval = 1.5f; 
-    [SerializeField] float projectileSpeed = 3f; 
+    [SerializeField] float fireInterval = 1.5f;
+    [SerializeField] float projectileSpeed = 3f;
 
     [Header("Spawn Offset (local XZ)")]
     [SerializeField] Vector2 offsetXZ = new Vector2(0f, 0.5f);
@@ -18,32 +18,36 @@ public class EnemyAttack : MonoBehaviour
     [SerializeField] float projectileLifetime = 2f;
 
     float nextFireTime;
+    EnemyDetectionApproach detector;
+
+    void Awake()
+    {
+        detector = GetComponent<EnemyDetectionApproach>();
+    }
 
     void Update()
     {
         if (!projectilePrefab || !player) return;
+        if (!detector || !detector.PlayerInRange) return;
+        if (!detector.AtContact) return;
 
-        if (Time.time >= nextFireTime)
+        if (Time.time < nextFireTime) return;
+        nextFireTime = Time.time + fireInterval;
+
+        Vector3 toPlayer = (player.position - transform.position).normalized;
+        Quaternion rot = Quaternion.LookRotation(toPlayer, Vector3.up);
+        Vector3 spawnPos = transform.position + rot * new Vector3(offsetXZ.x, 0f, offsetXZ.y);
+
+        GameObject go = Instantiate(projectilePrefab, spawnPos, rot);
+
+        var dpt = go.GetComponent<DamagePlayerOnTrigger>();
+        if (dpt) dpt.damage = enemyType ? enemyType.damageToPlayer : 1;
+
+        var fire = go.GetComponent<FireAttack>();
+        if (fire)
         {
-            nextFireTime = Time.time + fireInterval;
-
-            Vector3 toPlayer = (player.position - transform.position).normalized;
-            Quaternion rot = Quaternion.LookRotation(toPlayer, Vector3.up);
-
-            Vector3 spawnPos = transform.position
-                             + rot * new Vector3(offsetXZ.x, 0f, offsetXZ.y);
-
-            GameObject go = Instantiate(projectilePrefab, spawnPos, rot);
-
-            var dpt = go.GetComponent<DamagePlayerOnTrigger>();
-            if (dpt) dpt.damage = enemyType ? enemyType.damageToPlayer : 1;
-
-            var fire = go.GetComponent<FireAttack>();
-            if (fire)
-            {
-                fire.Launch(projectileSpeed, gameObject);
-                if (projectileLifetime > 0f) Destroy(go, projectileLifetime);
-            }
+            fire.Launch(projectileSpeed, gameObject);
+            if (projectileLifetime > 0f) Destroy(go, projectileLifetime);
         }
     }
 }
--- Assets/DoneScript/EnemyDetectionApproach.cs
+++ Assets/DoneScript/EnemyDetectionApproach.cs
@@ -1,5 +1,3 @@
-using NUnit.Framework;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDetectionApproach : MonoBehaviour
@@ -8,29 +6,33 @@ public class EnemyDetectionApproach : MonoBehaviour
     [SerializeField] float radius = 5.0f;
     [SerializeField] float movementSpeed = 2.0f;
     [SerializeField] float playerRadius = 1.0f;
+    [SerializeField] float contactBuffer = 0.15f;
+
+    public bool PlayerInRange { get; private set; }
+    public bool AtContact { get; private set; }
+    public float CurrentDistance { get; private set; }
+    public float ContactRadius => playerRadius;
 
     void Update()
     {
         Vector3 playerPos = player.transform.position;
         Vector3 ownPos = transform.position;
-
         Vector3 difference = playerPos - ownPos;
-        Vector3 d2 = new Vector2(difference.x, difference.z);
-
+        Vector2 d2 = new Vector2(difference.x, difference.z);
         float dotDistance = Vector2.Dot(d2, d2);
         float r2 = radius * radius;
 
-        if (dotDistance < r2)
+        CurrentDistance = Mathf.Sqrt(dotDistance);
+        PlayerInRange = dotDistance < r2;
+        AtContact = CurrentDistance <= playerRadius + contactBuffer;
+
+        if (PlayerInRange && !AtContact)
         {
-            float dist = Mathf.Sqrt(dotDistance);
-            if (dist > playerRadius + 1e-4f)
-            {
-                Vector2 dir2 = d2 / dist;
-                float maxStep = movementSpeed * Time.deltaTime;
-                float step = Mathf.Min(maxStep, dist - playerRadius);
-                Vector3 delta = new Vector3(dir2.x, 0f, dir2.y) * step;
-                transform.position += delta;
-            }
+            Vector2 dir2 = d2 / CurrentDistance;
+            float maxStep = movementSpeed * Time.deltaTime;
+            float step = Mathf.Min(maxStep, CurrentDistance - playerRadius);
+            Vector3 delta = new Vector3(dir2.x, 0f, dir2.y) * step;
+            transform.position += delta;
         }
     }
 }
